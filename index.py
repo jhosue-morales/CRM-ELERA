fef817b11841c5ac031cb5f790969dbc993c97e8
unknown <jhosueisaac09@gmail.com>
2026-09-02 22:52:09 -0500

    edicion 3

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 print("hola mundo")
-<<<<<<< HEAD
+
 
 
 a = input(print('Ingrese primer número \n » '))
@@ -9,8 +9,9 @@ b = input(print('Ingrese segundo número \n » '))
 def suma(a,b):
     blancSpace
     return a + b
-=======
+
+suma(a,b)
 h = int(1)
 o = int(5)
 print(h+o)
->>>>>>> 9b3e53f2e8f421564b947200da09cd7bd0c4b342
+
