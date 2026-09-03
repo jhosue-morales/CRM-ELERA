ad5467c06c6511aedb0a52ac71730e7165b00934
DianaM-C <dianamorancarranza@gmail.com>
2026-09-02 22:42:01 -0500

    Prueba de edicion con código .py no.2 edición sobreescrita

--- index.py
+++ index.py
@@ -1,7 +1,5 @@
 print("hola mundo")
 
-
-
 a = input(print('Ingrese primer número \n » '))
 blancSpace = print('')
 b = input(print('Ingrese segundo número \n » '))
@@ -10,8 +8,8 @@ def suma(a,b):
     blancSpace
     return a + b
 
-suma(a,b)
+
+
 h = int(1)
 o = int(5)
 print(h+o)
-
