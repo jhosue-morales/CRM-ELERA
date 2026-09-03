6872fb5bf79c18951fee14fb0705af53076b4dfd
DianaM-C <dianamorancarranza@gmail.com>
2026-09-02 22:31:05 -0500

    Prueba de edicion con código .py

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
 print("hola mundo")
+
+
+a = input(print('Ingrese primer número \n » '))
+blancSpace = print('')
+b = input(print('Ingrese segundo número \n » '))
+
+def suma(a,b):
+    blancSpace
+    return a + b
