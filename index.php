b44b1450d192ee440c676d14988c7f278e962be7
jhosue-morales <87512979+jhosue-morales@users.noreply.github.com>
2026-09-04 16:08:55 -0500

    30

--- index.php
+++ index.php
@@ -255,7 +255,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <div class="login-box"> 
                     <!-- LOGO MOBILE --> 
                     <div class="left-logo">
-                        <img src="/icono.jpg" alt="" srcset="" width="100%">
+                        <img src="/" alt="" srcset="" width="100%">
                     </div> 
                     <div class="mb-4"> 
                         <h2 class="login-title mb-2"> Bienvenido </h2> 
